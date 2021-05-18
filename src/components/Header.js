7a1e7509f923561e0ac127d22cddf66a1446ba65
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-05-18 01:25:20 -0500

    added NavTabs to act act navbar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NavTabs from "./NavTabs";
 import "../styles/styles.css";
 import pic from '../images/Bit1.PNG';
 
@@ -7,15 +8,11 @@ function Header() {
     <header className="subnav-hero-section">
       <div className="my-hero-container">
         <h2 className="subnav-hero-headling my-heading">Hi, I'm Coleen</h2>
-        <img className="my-pic"src={pic} alt="Profile Picture" />
+        <img className="my-pic" src={pic} alt="Profile" />
         <h5 className="my-htext">I'm a full-stack web developer</h5>
         <h5 className="my-htext">Welcome to my profile</h5>
       </div>
-      <ul class="subnav-hero-subnav">
-        <li><a href="#my-work" class="is-active">Work</a></li>
-        <li><a href="#about-me">About</a></li>
-        <li><a href="#contact-me">Contact</a></li>
-      </ul>
+      <NavTabs />
     </header>
     )
 }
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,49 +2,22 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function NavTabs() {
-  // track the current URL with location
+  // track the current URL with lo
   const location = useLocation();
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
+    <ul className="subnav-hero-subnav">
+      <li className="is-active">
+        <Link to="/">Work</Link>
       </li>
-      <li className="nav-item">
-        <Link
-          to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/blog"
-          className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
-        >
-          Blog
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact/learn"
-          className={location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
-        >
-          Learn
-        </Link>
+      <li>
+        <Link to="/about">About</Link>
+      </li>  
+      <li>
+        <Link to="/contact">Contact</Link>
       </li>
     </ul>
+
   );
 }
 
